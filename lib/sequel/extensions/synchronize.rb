6e873db1f2b8fe7fe7a21af5bab4d3dfcc036d2f
Fox <why@not.com>
2017-11-07 12:35:02 +0300

    renamed conflicting method

--- lib/sequel/extensions/synchronize.rb
+++ lib/sequel/extensions/synchronize.rb
@@ -6,7 +6,7 @@ module Sequel
   module Synchronize
     def synchronize_with(*args, timeout: 10.seconds, savepoint: false, key: lock_key_for(args))
       transaction(savepoint: savepoint) do
-        hash = hash(key)
+        hash = hash_for(key)
         get_lock(key, hash, timeout: timeout)
         Rails.logger.info("locked with #{key} (#{hash})")
         yield
@@ -25,7 +25,7 @@ module Sequel
       args.to_a.flatten.join("-")
     end
 
-    def hash(key)
+    def hash_for(key)
       Digest::MD5.hexdigest(key)[0..7].hex
     end
   end
